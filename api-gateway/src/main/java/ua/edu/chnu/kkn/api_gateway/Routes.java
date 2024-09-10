5014bc138b34d0d747b7cb9d5a198bbfc2836a1e
Edgar Vatamanitsa <e.vatamanitsa@chnu.edu.ua>
2024-09-10 12:46:01 +0300

    [5] Added api gateway project.

---
+++ api-gateway/src/main/java/ua/edu/chnu/kkn/api_gateway/Routes.java
@@ -0,0 +1,6 @@
+package ua.edu.chnu.kkn.api_gateway;
+
+
+public class Routes {
+
+}
---
+++ api-gateway/src/test/java/ua/edu/chnu/kkn/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.chnu.kkn.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
