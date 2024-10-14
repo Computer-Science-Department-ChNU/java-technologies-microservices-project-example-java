6da497c59824be05936ffcef6e227884429a5afb
Edgar Vatamanitsa <e.vatamanitsa@chnu.edu.ua>
2024-10-14 10:38:09 +0300

    [11] Fixed execution of the api endpoints from agregated api docs..

--- product/src/main/java/ua/edu/chnu/kkn/product/config/OpenApiConfig.java
+++ product/src/main/java/ua/edu/chnu/kkn/product/config/OpenApiConfig.java
@@ -3,15 +3,24 @@ package ua.edu.chnu.kkn.product.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Value("${server.port}")
+    int port;
+
     @Bean
     public OpenAPI productServiceAPI() {
+        var server = new Server().url("http://localhost:" + port);
         return new OpenAPI()
+                .servers(List.of(server))
                 .info(new Info().title("Product Service API")
                         .description("This is the REST API for Product Service")
                         .version("v0.0.1")
